e3bf34660afd25ee5d94fa557beae8ce989ba6a3
Veeni <veenitkumar.mca2012@gmail.com>
2018-05-08 10:33:17 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,5 @@
 <?php
        
-	//define('SHOPIFY_APP_API_KEY', 'a1d8a4ace6eaa1e0241afa2dcd19132c');
-	//define('SHOPIFY_APP_SHARED_SECRET', 'fdf3bce4b18ab3c6bf490118795dda47');
-	
 	define('SHOPIFY_APP_API_KEY', 'ba6421ebae4dc7690dc14d55148c1bf2');
 	define('SHOPIFY_APP_SHARED_SECRET', '650562caca627c703e6c4568ead33539');
 	
